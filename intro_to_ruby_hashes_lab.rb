2d28820e1e3a9060d6393f918bd0c34d9017d5ad
Emily G <aspentree79@gmail.com>
2019-08-07 17:47:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,10 +31,15 @@ def read_from_hash(hash, key)
   hash[key]
 end
 
+# def update_counting_hash(hash, key)
+#   if hash[key]
+#    hash[key] += 1
+#  else
+#    hash[key] = 1
+# end
+# end
+
 def update_counting_hash(hash, key)
-  if hash[key]
-   hash[key] += 1
- else
-   hash[key] = 1
-end
+  hash[key] ? hash[key] += 1 : hash[key] = 1
+  hash
 end
